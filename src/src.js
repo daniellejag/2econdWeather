6bf14b3ca78d574adfe27338905960940ba52377
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-09-06 19:22:56 -0400

    Added One Call API

--- src/src.js
+++ src/src.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+console.log(response.data.daily); 
   let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
@@ -46,6 +47,14 @@ forecastHTML = forecastHTML +
 });
                 forecastHTML = forecastHTML + `</div>`;
                 forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "b3a8312e8813d91f8ac0edd65adaa9c3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 
@@ -72,7 +81,11 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+ 
+  getForecast(response.data.coord);
+
 }
+
 
 function search(city) {
   let apiKey = "b3a8312e8813d91f8ac0edd65adaa9c3";
@@ -148,4 +161,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
